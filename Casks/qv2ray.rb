c047017ba64e194e5962d883845f1008743a68bb
librenix <librenix@users.noreply.github.com>
2021-02-26 10:40:18 +0900

    Bump to 2.7.0-pre2

---
+++ Casks/qv2ray.rb
@@ -0,0 +1,26 @@
+cask "qv2ray" do
+  version "2.7.0-pre2"
+  sha256 "dbf97e22e8c8081cd1ac07b880e0c3dd03e3f34a9a390729c7a95bfc8b974647"
+
+  url "https://github.com/Qv2ray/Qv2ray/releases/download/v#{version}/Qv2ray.v#{version}.macOS-x64.dmg",
+      verified: "github.com/Qv2ray/Qv2ray/"
+  name "Qv2ray"
+  desc "V2Ray GUI client with extensive protocol support"
+  homepage "https://qv2ray.net/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on formula: "v2ray"
+  depends_on macos: ">= :mojave"
+
+  app "qv2ray.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.github.qv2ray.plist",
+    "~/Library/Preferences/qv2ray",
+    "~/Library/Saved Application State/com.github.qv2ray.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Homebrew Qv2ray Tap
+
+## How do I install these formulae?
+`brew install qv2ray/qv2ray/<formula>`
+
+Or `brew tap qv2ray/qv2ray` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
